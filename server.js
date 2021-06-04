74bf331a39b3ad512f69f2f0598317311ac78b09
Wade Facer <wadepfacer@gmail.com>
2021-06-04 12:35:13 -0600

    created db connection and query request

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// database route
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Alkaline132!',
+        database: 'election'
+    },
+    console.log(`Now connected to the election database!`)
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default error 404 response for undefined route
 app.use((req, res) => {
